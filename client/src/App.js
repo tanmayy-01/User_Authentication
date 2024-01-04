22dc79f14259099e8498844dce3312a9cc2cf2dd
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-01-04 17:29:18 +0530

    Components are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Username></Username>
+    },
+    {
+        path : '/register',
+        element : <Register></Register>
+    },
+    {
+        path : '/password',
+        element : <Password></Password>
+    },
+    {
+        path : '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path : '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path : '*',
+        element : <PageNotFound></PageNotFound>
+    },
+])
 
 export default function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
-  
+  return (
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
+}
